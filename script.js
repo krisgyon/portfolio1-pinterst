40f1c5269a69618361c8059b0e74b3ea5455e386
krisgyon <krisgyon28@gmail.com>
2023-09-10 00:24:21 +0700

    navbar selesai

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const navLink = Array.from(document.querySelectorAll("nav a.nav-link"));
+const offcanvas = document.querySelector(".offcanvas");
 
 navLink.forEach((n) =>
   n.addEventListener("click", function (e) {
-    if (e.target.conta) e.target.classList.add(".active");
+    e.preventDefault();
+    navLink.forEach((n) => n.classList.remove("active"));
+    e.target.classList.add("active");
   })
 );
+
+offcanvas.addEventListener("show.bs.offcanvas", () => {
+  offcanvas.classList.add("navActive");
+});
+
+offcanvas.addEventListener("hidden.bs.offcanvas", () => {
+  offcanvas.classList.remove("navActive");
+});
